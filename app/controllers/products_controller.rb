5da8040d9766780352c651c1a1fb9ed7e583462e
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-09-01 16:59:19 -0400

    add controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def test_app
+    render json: {message: "hello"}
+  end
+
+  def product_one
+    @product = Product.find_by(id: 1)
+    render template: "products/show"
+  end
 end
