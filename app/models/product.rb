17277f95e4849b562c7718e8cf903d1b027b1ae1
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-09-08 16:13:26 -0400

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :price, numerality: {greater_than: 0}
+  validates :description , presence: true
+  validates :description , length: { in: 10..500 }
+
   def is_discounted?
     if price <= 10
       return true
